86ae6eb4bc064925573d130ce4ed2ce0b93ca076
Derek Bennett <bnnder@netscape.net>
2019-12-09 01:13:56 -0800

    created queries to populate the burger tables and fields

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Gus's Burger", TRUE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Bob's Burger", TRUE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Beyond Meat", TRUE);
